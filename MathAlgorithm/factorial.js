f9591643f0637dca073935a84e6cbb6c10d4057a
tomide <tomideadeyanju@gmail.com>
2023-04-21 17:19:53 +0000

    factorial.js

---
+++ MathAlgorithm/factorial.js
@@ -0,0 +1,22 @@
+/*
+Solving for a given integer 'n' , find the factoral of the integer.
+To solve this
+A factorial is  the factors of a non negative integer n!
+
+example 
+3! = 3 x 2 x 1
+*/
+
+function factorial(n) {
+    let factor = 1 
+    for (let i = 2; i <= n; i++) // i is intialize to 2 because 1 is a known factor 
+    {
+        factor = factor * i
+    }
+    return factor
+}
+
+console.log(factorial(4))
+console.log(factorial(5))
+console.log(factorial(3))
+//time complexity is linear O(n)
--- MathAlgorithm/fibonacci.js
+++ MathAlgorithm/fibonacci.js
@@ -8,11 +8,13 @@ fibonacci(2) = [0, 1]
 
 function fibonnaci(n) {
     const fib = [0, 1] // this is a contant for fibonacci the well known number is [0,1]
-    for(let i = 2; i < n; i++){// i starts from 2 because 0 and 1 is known 
+    for(let i = 2; i < n; i++){// the for loop starts from the second index
         fib[i] = fib [i - 1] + fib[i - 2]
+        //this equation stands that to get the present fibonnacci former fib - 1 + former fib - 2 and stored into fib
     }
     return fib
 }
 console.log(fibonnaci(5));
 console.log(fibonnaci(7));
 console.log(fibonnaci(8));
+// Therefore the time complexity is linear O(n) this is because it has one loop and also run based on input 
